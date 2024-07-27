4b25f3d32293111db43e73a1bfdc846d9b2a24e7
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-07-27 22:57:58 +0500

    updated

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -170,8 +170,8 @@
             Close
         </button>
         <button type="button"
-                class="py-2 px-3 inline-flex items-center text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-            <x-icons.ok class="shrink-0 size-6"/>
+                class="gap-x-1 py-2 px-3 inline-flex items-center text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+            <x-icons.ok class="size-5"/>
             Save
         </button>
     </x-slot>
