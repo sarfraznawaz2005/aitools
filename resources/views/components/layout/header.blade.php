694204d916ec216850a71aa2bbbc376063793c97
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-07-29 14:35:41 +0500

    updated

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -189,21 +189,22 @@
                 <!-- End Floating Input -->
 
             </fieldset>
+
+            <div class="flex justify-end items-center gap-x-2 py-3 px-4">
+                <button type="button"
+                        class="py-2 px-3 inline-flex items-center text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                        data-hs-overlay="#general-modal">
+                    Close
+                </button>
+                <button type="submit"
+                        class="gap-x-1 py-2 px-3 inline-flex items-center text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                    <x-icons.ok class="size-5"/>
+                    Save
+                </button>
+            </div>
         </form>
     </x-slot>
 
-    <x-slot name="buttons">
-        <button type="button"
-                class="py-2 px-3 inline-flex items-center text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                data-hs-overlay="#general-modal">
-            Close
-        </button>
-        <button type="submit"
-                class="gap-x-1 py-2 px-3 inline-flex items-center text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-            <x-icons.ok class="size-5"/>
-            Save
-        </button>
-    </x-slot>
 
 </x-modal>
 
--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -13,9 +13,6 @@
             <div class="p-4 overflow-y-auto">
                 {{ $body }}
             </div>
-            <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
-                {{ $buttons }}
-            </div>
         </div>
     </div>
 </div>
