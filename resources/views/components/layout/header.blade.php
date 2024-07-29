d4a395fdc1d35f3e554d411529fa1f0c500aea6f
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-07-29 17:20:28 +0500

    updated

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -194,7 +194,7 @@
 
             </fieldset>
 
-            <div class="flex justify-end items-center gap-x-2 py-3 px-4">
+            <div class="flex justify-end items-center gap-x-4 pt-4">
                 <button type="button"
                         class="py-2 px-3 inline-flex items-center text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
                         data-hs-overlay="#general-modal">
