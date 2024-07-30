532ff6407ab713c034117568aa4bd466094f7e5e
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-07-30 22:42:41 +0500

    updated

--- resources/views/livewire/api-keys-form.blade.php
+++ resources/views/livewire/api-keys-form.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ llm_type: '' }">
 
     @if ($apiKeys && count($apiKeys))
-        <fieldset class="border border-gray-300 rounded-lg p-4 dark:border-neutral-700">
+        <fieldset class="border border-gray-300 rounded-lg p-4 dark:border-neutral-700 mb-4">
             <legend class="text-sm font-medium text-gray-500 dark:text-neutral-300">SAVED API KEYS</legend>
 
             <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
@@ -16,7 +16,7 @@
                         Type
                     </th>
                     <th scope="col"
-                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-neutral-300 text-center">
+                        class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-neutral-300">
                         Action
                     </th>
                 </tr>
@@ -24,13 +24,13 @@
                 <tbody class="bg-white divide-y divide-gray-200 dark:bg-neutral-700 dark:divide-neutral-600">
                 @foreach($apiKeys as $apiKey)
                     <tr wire:key="{{$apiKey->id}}">
-                        <td class="px-6 py-3 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-300">
+                        <td class="px-6 py-2 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-300">
                             {{ $apiKey->name }}
                         </td>
-                        <td class="px-6 py-3 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-300">
+                        <td class="px-6 py-2 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-300">
                             {{ strtoupper($apiKey->llm_type) }}
                         </td>
-                        <td class="px-6 py-3 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-300 text-center">
+                        <td class="px-6 py-2 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-300 text-center">
                             @if ($apiKey->active === 1)
                                 <button title="This is currently default"
                                         class="cursor-default items-center px-2 py-1 text-white bg-green-600 rounded mr-2">
@@ -65,7 +65,6 @@
             </table>
 
         </fieldset>
-        <br>
     @endif
 
     <x-flash/>
@@ -77,30 +76,29 @@
             <legend class="text-sm font-medium text-gray-500 dark:text-neutral-300">API KEY</legend>
 
             <!-- Select -->
-            <select x-model="llm_type" wire:model="llm_type" id="llm_type"
-                    class="py-3 px-4 pe-9 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600">
-                <option value="">Choose LLM</option>
-                <option value="openai">OpenAi</option>
-                <option value="gemini">Gemini</option>
-                <option value="ollama">Ollama</option>
-            </select>
+            <div class="relative mb-3">
+                <select x-model="llm_type" wire:model="llm_type" id="llm_type"
+                        class="py-3 px-4 pe-9 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600">
+                    <option value="">Choose LLM</option>
+                    <option value="openai">OpenAi</option>
+                    <option value="gemini">Gemini</option>
+                    <option value="ollama">Ollama</option>
+                </select>
+            </div>
             <!-- End Select -->
 
-            <br>
-
             <!-- Floating Input -->
-            <div class="relative" x-show="llm_type === 'ollama'">
+            <div class="relative mb-3" x-show="llm_type === 'ollama'">
                 <input type="url" wire:model="base_url" id="base_url"
                        class="peer p-4 block w-full bg-gray-100 border-transparent rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600 focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2">
                 <label for="base_url"
                        class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] dark:text-neutral-400 peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500 dark:peer-focus:text-neutral-500 peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500">Base
                     URL</label>
-                <br>
             </div>
             <!-- End Floating Input -->
 
             <!-- Floating Input -->
-            <div class="relative">
+            <div class="relative mb-3">
                 <input type="text" wire:model="api_key" id="api_key"
                        class="peer p-4 block w-full bg-gray-100 border-transparent rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600 focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2">
                 <label for="api_key"
@@ -108,8 +106,6 @@
                     Key</label>
             </div>
             <!-- End Floating Input -->
-
-            <br>
 
             <!-- Floating Input -->
             <div class="relative">
