a5f0b17ceb3e9862de53685ba30edacc0c1b2fb4
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-07-31 17:39:02 +0500

    updated

--- resources/views/components/confirm-dialog.blade.php
+++ resources/views/components/confirm-dialog.blade.php
@@ -12,9 +12,11 @@
 
     <div x-show="open" class="fixed inset-0 flex items-center justify-center z-[80]">
         <div class="fixed inset-0 bg-black opacity-50 z-[70]" @click="open = false"></div>
-        <div class="bg-white dark:bg-neutral-800 rounded-lg shadow-lg p-6 w-full max-w-sm mx-auto z-[80]" @click.stop>
-            <h2 class="text-lg font-semibold text-gray-800 dark:text-neutral-300 mb-4">Confirm Deletion</h2>
-            <p class="text-sm text-gray-600 dark:text-neutral-400 mb-6">Are you sure you want to delete this item?</p>
+        <div class="bg-white dark:bg-neutral-800 rounded-lg shadow-lg p-4 w-full max-w-sm mx-auto z-[80]" @click.stop>
+            <p class="text-lg text-gray-600 dark:text-neutral-400 mb-6 border-b border-gray-100 dark:border-neutral-700 pb-2">
+                Are you sure you want to delete this item?
+            </p>
+
             <div class="flex justify-end gap-4">
                 <button @click="open = false"
                         class="py-2 px-4 bg-gray-200 dark:bg-neutral-700 text-gray-800 dark:text-neutral-300 rounded-lg hover:bg-gray-300 dark:hover:bg-neutral-600">
