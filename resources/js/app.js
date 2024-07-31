9799a9a9ae0cff1070b375d390cca8d17cdbe887
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-07-31 15:59:48 +0500

    updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
 //import './bootstrap';
 import '../css/app.css';
 import 'preline';
+
+// This re-initializes the preline components. Need to do this only when using livewire's wire:navigate on links.
+document.addEventListener('livewire:navigated', () => {
+    window.HSStaticMethods.autoInit();
+})
