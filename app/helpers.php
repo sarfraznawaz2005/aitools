a938495753a8b01d2b38c15b8f2d98e0f4280910
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-05 21:06:36 +0500

    updated

--- app/helpers.php
+++ app/helpers.php
@@ -1,3 +1,16 @@
 <?php
 
-//
+use App\Models\ApiKey;
+use Sajadsdi\LaravelSettingPro\Support\Setting;
+
+function getChatBuddySelectedModel(): string
+{
+    if (
+        Setting::select('ChatBuddy')->has('selectedModel') &&
+        ApiKey::where('model_name', Setting::select('ChatBuddy')->get('selectedModel'))->exists()
+    ) {
+        return ApiKey::where('model_name', Setting::select('ChatBuddy')->get('selectedModel'))->first();
+    }
+
+    return ApiKey::where('active', true)->first();
+}
