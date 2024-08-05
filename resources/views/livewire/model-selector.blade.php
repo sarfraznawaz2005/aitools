957458940ae3b44a89a6ce3467d42bd1b886af0e
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-05 19:44:47 +0500

    updated

--- resources/views/livewire/model-selector.blade.php
+++ resources/views/livewire/model-selector.blade.php
@@ -3,7 +3,7 @@
         wire:model.change="selectedModel"
         class="py-3 px-4 pe-9 block w-full font-bold text-center bg-gray-100 border-transparent text-sm outline-0 focus:outline-none focus:ring-0 focus-visible:ring-0 focus:border-gray-400">
         @foreach($apiKeys as $apiKey)
-            <option value="{{ $apiKey->model_name }}">{{ $apiKey->model_name }}</option>
+            <option value="{{ $apiKey->model_name }}" {{$selectedModel === $apiKey->model_name ? 'selected' : ''}}>{{ $apiKey->model_name }}</option>
         @endforeach
     </select>
 </div>
