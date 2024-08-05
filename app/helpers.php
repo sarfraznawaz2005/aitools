9a07e7ce7ee66e46048325652743bfb479ea6831
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-05 21:02:41 +0500

    updated

--- app/helpers.php
+++ app/helpers.php
@@ -1,12 +1,3 @@
 <?php
 
-use App\Models\ApiKey;
-
-function getActiveModel(): string
-{
-    if (session('selectedModel') && ApiKey::where('model_name', session('selectedModel'))->exists()) {
-        return ApiKey::where('model_name', session('selectedModel'))->first();
-    }
-
-    return ApiKey::where('active', true)->first();
-}
+//
