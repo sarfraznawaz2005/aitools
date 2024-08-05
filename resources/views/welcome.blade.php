2de6f2e63d6d6fd853cf8519b45ea3a0a6a1798c
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-05 10:53:21 +0500

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
                 <a href="{{route($tool['route'])}}" wire:navigate>
                     <div class="size-full bg-white shadow-lg rounded-lg p-4 dark:bg-neutral-900 text-center hover:bg-gray-200">
                         <div class="inline-flex justify-center items-center">
-                            <x-dynamic-component :component="'icons.' . $tool['icon']['name']" :color="$tool['icon']['color']" class="shrink-0 size-12"/>
+                            <x-dynamic-component :component="'icons.' . $tool['icon']['name']" :color="$tool['icon']['color']" class="shrink-0 size-16"/>
                         </div>
                         <h3 class="block text-lg font-bold text-gray-800 dark:text-white">{{$tool['name']}}</h3>
                         <p class="text-gray-600 dark:text-neutral-400">{{$tool['description']}}</p>
