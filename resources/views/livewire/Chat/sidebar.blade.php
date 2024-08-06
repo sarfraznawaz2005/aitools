4b78ad28713f54ccb2e25230d64f1bc55aa12fa3
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-06 18:44:34 +0500

    updated

--- resources/views/livewire/Chat/sidebar.blade.php
+++ resources/views/livewire/Chat/sidebar.blade.php
@@ -43,14 +43,14 @@
                             <ul class="py-1">
                                 <li>
                                     <a href="#"
-                                       class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-800">
+                                       class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-800">
                                         <x-icons.edit class="inline-block mr-2 text-gray-500"/>
                                         Rename
                                     </a>
                                 </li>
                                 <li>
                                     <a href="#"
-                                       class="block px-4 py-2 text-xs text-red-600 hover:bg-gray-100 dark:text-red-400 dark:hover:bg-neutral-800">
+                                       class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:text-red-400 dark:hover:bg-neutral-800">
                                         <x-icons.delete class="inline-block mr-2 text-red-500"/>
                                         Delete
                                     </a>
