b3db5a3d2c8bdd8b251ad63a7cdec7e56671e588
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-06 11:08:35 +0500

    updated

--- resources/views/livewire/model-selector.blade.php
+++ resources/views/livewire/model-selector.blade.php
@@ -1,9 +1,14 @@
 <div>
-    <select
-        wire:model.change="selectedModel"
-        class="py-3 px-4 pe-9 block w-full font-bold text-center bg-gray-100 border-transparent text-sm outline-0 focus:outline-none focus:ring-0 focus-visible:ring-0 focus:border-gray-400">
-        @foreach($apiKeys as $apiKey)
-            <option value="{{ $apiKey->model_name }}" {{$selectedModel === $apiKey->model_name ? 'selected' : ''}}>{{ $apiKey->model_name }}</option>
+    <select wire:model.change="selectedModel"
+            class="py-3 px-4 pe-9 block w-full font-bold text-center bg-gray-100 border-transparent text-sm outline-0 focus:outline-none focus:ring-0 focus-visible:ring-0 focus:border-gray-400">
+        @foreach($apiKeys->groupBy('llm_type') as $llmType => $groupedApiKeys)
+            <optgroup label="{{ $llmType }}">
+                @foreach($groupedApiKeys as $apiKey)
+                    <option
+                        value="{{ $apiKey->model_name }}" {{$selectedModel === $apiKey->model_name ? 'selected' : ''}}>{{ $apiKey->model_name }}
+                    </option>
+                @endforeach
+            </optgroup>
         @endforeach
     </select>
 </div>
