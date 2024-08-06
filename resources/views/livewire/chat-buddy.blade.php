f358317f03e7d5e890b7805b41211e696764e248
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-06 12:02:20 +0500

    updated

--- resources/views/livewire/chat-buddy.blade.php
+++ resources/views/livewire/chat-buddy.blade.php
@@ -41,13 +41,13 @@
                         <div x-cloak x-show="open" @click.away="open = false" class="absolute right-[-10px] bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-neutral-900 dark:border-neutral-700">
                             <ul class="py-1">
                                 <li>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-800">
+                                    <a href="#" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-800">
                                         <x-icons.edit class="inline-block mr-2 text-gray-500" />
                                         Rename
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:text-red-400 dark:hover:bg-neutral-800">
+                                    <a href="#" class="block px-4 py-2 text-xs text-red-600 hover:bg-gray-100 dark:text-red-400 dark:hover:bg-neutral-800">
                                         <x-icons.delete class="inline-block mr-2 text-red-500" />
                                         Delete
                                     </a>
