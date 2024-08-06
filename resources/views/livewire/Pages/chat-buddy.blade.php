d02e8e9348b23f9decc242c9f5a9b05a8a583270
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-06 19:25:25 +0500

    updated

--- resources/views/livewire/Pages/chat-buddy.blade.php
+++ resources/views/livewire/Pages/chat-buddy.blade.php
@@ -1,7 +1,7 @@
 <div>
     <livewire:chat.sidebar/>
 
-    <div class="relative h-screen w-full lg:ps-64">
+    <div class="relative">
         <livewire:chat.chatlist :conversation="$conversation"/>
 
         <livewire:chat.chatinput :conversation="$conversation"/>
