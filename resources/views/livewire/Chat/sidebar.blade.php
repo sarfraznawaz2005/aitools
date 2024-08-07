e0d3f99c34a24c2636cd8d92150b6da5dbe315ce
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-07 13:35:11 +0500

    updated

--- resources/views/livewire/Chat/sidebar.blade.php
+++ resources/views/livewire/Chat/sidebar.blade.php
@@ -2,9 +2,9 @@
 <div id="hs-application-sidebar"
      class="hs-overlay [--auto-close:lg] hs-overlay-open:translate-x-0 -translate-x-full duration-300 transform hidden fixed top-14 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-neutral-900 dark:border-neutral-700"
      role="dialog" tabindex="-1" aria-label="Sidebar" x-data="{ openDropdown: null }">
-    <nav class="size-full flex flex-col">
+    <nav class="size-full flex flex-col h-full">
 
-        <div class="h-full">
+        <div class="h-full overflow-y-auto flex-1">
 
             <!-- List -->
             <ul class="space-y-1.5 p-4">
@@ -65,10 +65,8 @@
         </div>
 
         <!-- Footer -->
-        <div class="relative">
-            <div class="border-t border-gray-200 dark:border-neutral-700 fixed bottom-0 w-full">
-                <livewire:general.model-selector for="ChatBuddy" />
-            </div>
+        <div class="sticky bottom-0 border-t-2 border-gray-200 dark:border-neutral-700 bg-gray-200 dark:bg-neutral-900 p-1">
+            <livewire:general.model-selector for="ChatBuddy" />
         </div>
         <!-- End Footer -->
     </nav>
