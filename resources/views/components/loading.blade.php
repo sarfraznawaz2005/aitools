8a9d5e12ce1ab293568fd35b718f518a4c741ff7
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-12 19:09:56 +0500

    updated

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -22,7 +22,7 @@
     wire:ignore
     x-cloak
     class="fixed inset-0 flex items-center justify-center z-[1000]">
-    <div class="fixed inset-0 bg-black opacity-5 z-[70]"></div>
+    <div class="fixed inset-0 bg-transparent opacity-5 z-[70]"></div>
     <div class="animate-spin size-12 border-[5px] border-current border-t-transparent text-blue-600 rounded-full">
         <span class="sr-only">Loading...</span>
     </div>
