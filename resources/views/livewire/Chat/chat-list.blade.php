7815b59118fab33bebe20170c6f6967c90b78a2c
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-14 13:42:55 +0500

    updated

--- resources/views/livewire/Chat/chat-list.blade.php
+++ resources/views/livewire/Chat/chat-list.blade.php
@@ -129,10 +129,6 @@
                     @endif
 
                 @empty
-                    <li
-                        class="fixed inset-0 m-auto w-full lg:left-32 h-64 flex items-center justify-center text-gray-300 text-3xl font-bold">
-                        Start New Conversation
-                    </li>
                 @endforelse
             @else
                 <li
