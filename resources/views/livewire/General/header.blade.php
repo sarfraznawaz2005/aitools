dc63bc95205b2fe96758272b39bf3376cb65b60f
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-14 13:32:39 +0500

    updated

--- resources/views/livewire/General/header.blade.php
+++ resources/views/livewire/General/header.blade.php
@@ -10,7 +10,7 @@
         <nav class="flex basis-full items-center w-full mx-auto">
             <div class="w-full flex items-center ms-auto justify-between">
 
-                <div class="inline-flex items-center">
+                <div class="inline-flex items-center py-0.5">
 
                     <a href="{{route('home')}}" wire:navigate>
                         <div class="inline-flex items-center px-4 ml-1 mt-1">
@@ -84,11 +84,11 @@
 
                     <button type="button"
                             x-data x-tooltip.raw="Settings"
-                            class="size-[38px] relative inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                            class="size-[38px] relative inline-flex justify-center items-center gap-x-2 mr-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
                             aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-basic-modal"
                             data-hs-overlay="#general-modal">
                         <x-icons.settings
-                            class="text-gray-500 dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-500"/>
+                            class="text-gray-500 hover:text-gray-700"/>
                         <span class="sr-only">Settings</span>
                     </button>
 
