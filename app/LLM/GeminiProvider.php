4cfbb248cbed9c4ed8b73c3e0b7657d9fc8f94fa
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-15 13:34:56 +0500

    updated

--- app/LLM/GeminiProvider.php
+++ app/LLM/GeminiProvider.php
@@ -53,31 +53,20 @@ class GeminiProvider extends BaseLLMProvider
                 } catch (Exception) {
                     // fallback via non-streaming response
                     sleep(1);
+                    $response = $this->makeRequest($this->baseUrl . 'models/' . $this->model . ":generateContent?key=" . $this->apiKey, $body, false, false, $callback);
+                    $text = $this->getResult($response);
 
-                    try {
-                        $response = $this->makeRequest($this->baseUrl . 'models/' . $this->model . ":generateContent?key=" . $this->apiKey, $body, false, false, $callback);
-                        $text = $this->getResult($response);
-
-                        if ($callback) {
-                            $callback($text);
-                        } else {
-                            echo "event: update\n";
-                            echo 'data: ' . json_encode($text) . "\n\n";
-                            ob_flush();
-                            flush();
-                        }
-                    } catch (Exception $e) {
-                        throw new Exception($e->getMessage());
+                    if ($callback) {
+                        $callback($text);
+                    } else {
+                        echo "event: update\n";
+                        echo 'data: ' . json_encode($text) . "\n\n";
+                        ob_flush();
+                        flush();
                     }
-
-
                 }
             } else {
-                try {
-                    $response = $this->makeRequest($url, $body, false, false, $callback);
-                } catch (Exception $e) {
-                    throw new Exception($e->getMessage());
-                }
+                $response = $this->makeRequest($url, $body, false, false, $callback);
             }
 
             return isset($response) ? $this->getResult($response) : '';
