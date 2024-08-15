d01c147fba69dd5efbb477f3ace1152be126b2c7
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-15 18:36:31 +0500

    updated

--- resources/views/livewire/ApiKeys/api-keys-form.blade.php
+++ resources/views/livewire/ApiKeys/api-keys-form.blade.php
@@ -126,7 +126,6 @@
         <div class="relative mb-3" x-show="$wire.llm_type === 'Ollama'">
             <input type="url" wire:model="base_url"
                    id="base_url"
-                   placeholder="http://127.0.0.1:11434"
                    class="peer p-4 block w-full bg-gray-100 border-transparent rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600 focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2">
             <label for="base_url"
                    class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] dark:text-neutral-400 peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500 dark:peer-focus:text-neutral-500 peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500">Base
