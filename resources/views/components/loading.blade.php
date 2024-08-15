c1a6bb68f193f4c07f3344a09a7a3fca9d1bf621
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-15 21:29:57 +0500

    updated

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -2,19 +2,10 @@
     document.addEventListener('alpine:init', () => {
         Alpine.data('loading', () => ({
             show: false,
-            bgColor: 'bg-yellow-400',
-            opacity: 'opacity-75',
 
             init() {
-                Livewire.on('showLoading', (bgColor, opacity) => {
-                    this.bgColor = bgColor || this.bgColor;
-                    this.opacity = opacity || this.opacity;
-                    this.show = true;
-                });
-
-                Livewire.on('hideLoading', () => {
-                    this.show = false;
-                });
+                Livewire.on('showLoading', () => this.show = true);
+                Livewire.on('hideLoading', () => this.show = false);
             }
         }))
     });
@@ -28,16 +19,17 @@
     class="fixed inset-0 flex items-center justify-center z-[1000]">
     <div class="fixed inset-0 bg-transparent opacity-5 z-[70]"></div>
 
-    <div class="relative">
-        <span class="flex absolute size-10 right-0">
-            <span :class="`animate-ping absolute inline-flex size-full rounded-full ${bgColor} ${opacity}`"></span>
-            <!--<span class="relative inline-flex rounded-full size-10 bg-green-500"></span>-->
-        </span>
-    </div>
-
-    {{--
-    <div class="animate-spin size-12 border-[5px] border-current border-t-transparent text-blue-600 rounded-full">
-        <span class="sr-only">Loading...</span>
-    </div>
-    --}}
+    <span
+        class="mb-4 animate-ping inline-flex justify-center items-center size-12 rounded-full border-4 border-green-50 bg-green-100 text-green-500 dark:bg-green-700 dark:border-green-600 dark:text-green-100"
+    >
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="shrink-0 size-8"
+              fill="currentColor"
+              viewBox="0 0 16 16">
+            <path
+                d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"
+            />
+          </svg>
+    </span>
 </div>
