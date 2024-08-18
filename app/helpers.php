1bc6c6687bff93b30cc7a2b481a0b6929aabe748
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-18 16:56:20 +0500

    updated

--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,6 @@
 /*
  * TODO
  * global disabler until api key is saved
- * more icons
  * cache https://livewire.laravel.com/docs/computed-properties#caching-between-requests
  * Tips notifier should support multiple tips
  * chat with pdf
