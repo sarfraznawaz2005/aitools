95971826b6d25a0c4bac3560029dfc8b4544b6aa
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-18 19:26:03 +0500

    updated

--- resources/views/livewire/Chat/bot-selector.blade.php
+++ resources/views/livewire/Chat/bot-selector.blade.php
@@ -16,6 +16,45 @@
             <div class="w-full flex justify-center items-center flex-wrap">
                 <fieldset
                     class="items-center justify-center font-semibold w-full border border-gray-300 rounded-lg p-5 pb-7 dark:border-neutral-700 my-4">
+                    <legend class="text-sm text-gray-500 dark:text-neutral-300">System Bots</legend>
+
+                    <div
+                        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 font-medium">
+                        @foreach($bots as $bot)
+                            @if($bot->system)
+                                <button
+                                    type="button"
+                                    wire:click.prevent="selectBot({{ $bot->id }})"
+                                    x-data="{
+                                   isSelected: false,
+                                    init() {
+                                            $wire.on('botSelected', (botId) => {
+                                                this.isSelected = (botId == {{ $bot->id }});
+                                            });
+                                        }
+                                    }"
+                                    :class="{ 'bg-yellow-100 hover:bg-yellow-100': isSelected }"
+                                    x-tooltip.raw="{{$bot->bio}}"
+                                    wire:key="bot-{{ $bot->id }}"
+                                    x-ref="button"
+                                    class="w-full text-sm inline-flex items-center rounded-lg border border-gray-300 text-gray-800 hover:bg-gray-200 focus:outline-none disabled:opacity-50 disabled:pointer-events-none"
+                                >
+
+                                    <span class="inline-flex mr-2 justify-center items-center p-1 rounded-lg">
+                                      <span class="inline-block text-base lg:text-2xl md:text-2xl xl:text-2xl">
+                                          {{ $bot->icon }}
+                                      </span>
+                                    </span>
+
+                                    <span class="truncate">{{ $bot->name }}</span>
+                                </button>
+                            @endif
+                        @endforeach
+                    </div>
+                </fieldset>
+
+                <fieldset
+                    class="items-center justify-center font-semibold w-full border border-gray-300 rounded-lg p-5 pb-7 dark:border-neutral-700 my-4">
                     <legend class="text-sm text-gray-500 dark:text-neutral-300">Your Bots</legend>
 
                     <div
@@ -76,45 +115,6 @@
                         </x-gradient-button>
                     </div>
 
-                </fieldset>
-
-                <fieldset
-                    class="items-center justify-center font-semibold w-full border border-gray-300 rounded-lg p-5 pb-7 dark:border-neutral-700 my-4">
-                    <legend class="text-sm text-gray-500 dark:text-neutral-300">System Bots</legend>
-
-                    <div
-                        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 font-medium">
-                        @foreach($bots as $bot)
-                            @if($bot->system)
-                                <button
-                                    type="button"
-                                    wire:click.prevent="selectBot({{ $bot->id }})"
-                                    x-data="{
-                                   isSelected: false,
-                                    init() {
-                                            $wire.on('botSelected', (botId) => {
-                                                this.isSelected = (botId == {{ $bot->id }});
-                                            });
-                                        }
-                                    }"
-                                    :class="{ 'bg-yellow-100 hover:bg-yellow-100': isSelected }"
-                                    x-tooltip.raw="{{$bot->bio}}"
-                                    wire:key="bot-{{ $bot->id }}"
-                                    x-ref="button"
-                                    class="w-full text-sm inline-flex items-center rounded-lg border border-gray-300 text-gray-800 hover:bg-gray-200 focus:outline-none disabled:opacity-50 disabled:pointer-events-none"
-                                >
-
-                                    <span class="inline-flex mr-2 justify-center items-center p-1 rounded-lg">
-                                      <span class="inline-block text-base lg:text-2xl md:text-2xl xl:text-2xl">
-                                          {{ $bot->icon }}
-                                      </span>
-                                    </span>
-
-                                    <span class="truncate">{{ $bot->name }}</span>
-                                </button>
-                            @endif
-                        @endforeach
-                    </div>
                 </fieldset>
             </div>
 
