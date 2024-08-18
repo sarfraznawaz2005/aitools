0a1b256bfa3929201482867be6d651a9c925faad
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-19 02:25:53 +0500

    updated

--- resources/views/livewire/Chat/bot-selector.blade.php
+++ resources/views/livewire/Chat/bot-selector.blade.php
@@ -102,7 +102,7 @@
                         @endforeach
                     </div>
 
-                    <div class="flex justify-center {{$bots->where('system', true)->count() > 0 ? 'mt-8' : ''}}">
+                    <div class="flex justify-center {{$bots->where('system', false)->count() > 0 ? 'mt-8' : ''}}">
                         <x-gradient-button data-hs-overlay="#botModal" wire:click="resetForm">
                             <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
