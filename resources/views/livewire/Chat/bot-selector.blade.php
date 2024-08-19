214b5a8a92f81e7d736dd89d46b9d6866771a203
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-19 11:08:43 +0500

    updated

--- resources/views/livewire/Chat/bot-selector.blade.php
+++ resources/views/livewire/Chat/bot-selector.blade.php
@@ -177,9 +177,7 @@
                                                 x-transition:leave-end="opacity-0 transform scale-95"
                                                 class="hs-tooltip-content text-wrap p-4 hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible hidden opacity-0 transition-opacity absolute invisible z-[100] max-w-xs w-full bg-white border border-gray-100 text-start rounded-xl shadow-md after:absolute after:top-0 after:-start-4 after:w-4 after:h-full"
                                                 role="tooltip">
-                                                Tip: You can use special word <span class="text-green-600 text-xs">@{{USER_QUESTION}}</span> to insert user's question at specific place in your prompt.
-                                                <hr class="h-1 m-0 my-2">
-                                                <div class="font-semibold mb-2 text-xs">Prompt Example:</div>
+                                                <span class="font-semibold mb-2 text-xs">Prompt Example:</span><hr class="h-1 my-2">
                                                 I want you to act as an interviewer. I will be the candidate and you will ask me the interview questions for the position position. I want you to only reply as the interviewer. Do not write all the conservation at once. I want you to only do the interview with me. Ask me the questions and wait for my answers. Do not write explanations. Ask me the questions one by one like an interviewer does and wait for my answers.
                                             </span>
                                         </span>
