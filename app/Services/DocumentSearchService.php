584ce77b26ac8a0f3cf443ce4a3ae6f14d7299ac
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-20 22:38:56 +0500

    updated

--- app/Services/DocumentSearchService.php
+++ app/Services/DocumentSearchService.php
@@ -13,7 +13,7 @@ class DocumentSearchService
     public function __construct(
         protected LlmProvider $llm,
         protected string      $fileIdentifier,
-        protected int         $chunkSize = 1000,
+        protected int         $chunkSize = 500,
         protected float       $similarityThreshold = 0.6,
         protected int         $maxResults = 3,
     )
