d546f6da099bdf439d514fadd87fd702995f1416
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-22 01:56:33 +0500

    updated

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,6 @@
 <?php
 /*
  * TODO
- * global disabler until api key is saved
  * openai/ollama chat pdf
  * cache https://livewire.laravel.com/docs/computed-properties#caching-between-requests
  * Tips notifier should support multiple tips
