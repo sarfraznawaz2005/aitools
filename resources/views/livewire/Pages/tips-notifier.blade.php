fce237bc848b6dea8af28a4dd5e238f266eca5eb
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-22 23:41:17 +0500

    updated

--- resources/views/livewire/Pages/tips-notifier.blade.php
+++ resources/views/livewire/Pages/tips-notifier.blade.php
@@ -22,7 +22,7 @@
             </label>
 
             {{$schedule_type}}
-            <select id="schedule_type" wire:model="schedule_type"
+            <select id="schedule_type" wire:model.change="schedule_type"
                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo 200 focus:ring-opacity-50">
                 <option value="every_minute">Every Minute</option>
                 <option value="hourly">Hourly</option>
