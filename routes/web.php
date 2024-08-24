4d5319cf5968cca9b19580ca64cb977633aa601d
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-24 17:22:03 +0500

    updated

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,15 @@ Route::get('/', function () {
 //    Artisan::call('schedule:run');
 //});
 
-Route::get('test', function () {
+Route::get('/test', function () {
     return view('test');
 })->name('test');
 
-Route::get($tools['chat-buddy']['route'], ChatBuddy::class)->name($tools['chat-buddy']['route']);
-Route::get($tools['chat-buddy']['route'] . '/{conversation}', ChatBuddy::class)->name($tools['chat-buddy']['route'] . 'load-conversation');
+Route::get('/' . $tools['chat-buddy']['route'], ChatBuddy::class)->name($tools['chat-buddy']['route']);
+Route::get('/' . $tools['chat-buddy']['route'] . '/{conversation}', ChatBuddy::class)->name($tools['chat-buddy']['route'] . 'load-conversation');
 Route::get('/chat-buddy/chat/{conversation}', [ChatBuddy::class, 'chat']);
 
-Route::get($tools['text-styler']['route'], TextStyler::class)->name($tools['text-styler']['route']);
+Route::get('/' . $tools['text-styler']['route'], TextStyler::class)->name($tools['text-styler']['route']);
 Route::get('/text-styler/chat', [TextStyler::class, 'chat']);
 
-Route::get($tools['tips-notifier']['route'], TipsNotifier::class)->name($tools['tips-notifier']['route']);
+Route::get('/' . $tools['tips-notifier']['route'], TipsNotifier::class)->name($tools['tips-notifier']['route']);
