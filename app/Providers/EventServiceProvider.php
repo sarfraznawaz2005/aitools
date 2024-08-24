db9bdc53dbc5ced68224daad9f9251d2945ab649
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-24 23:08:37 +0500

    updated

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,13 +5,18 @@ namespace App\Providers;
 use App\Events\OnNotificationShown;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 
 class EventServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         Event::listen(OnNotificationShown::class, function () {
-            openWindow('tip', 'tips-notifier', false);
+            Log::info('Opening Tips Window');
+
+            sleep(1);
+
+            openWindow('tip', 'tips-notifier');
             //Window::close('main');
         });
     }
