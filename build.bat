0425cf6fe5746c372a72b0081b95df688813e62c
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-26 23:14:48 +0500

    updated

--- build.bat
+++ build.bat
@@ -1,8 +1,3 @@
-rem do not use this command as it will delete existing data of other users, but works in my case.
-php artisan native:migrate:fresh
-
-php artisan native:db:seed
-
 rem now build the app
 php artisan native:build win
 
