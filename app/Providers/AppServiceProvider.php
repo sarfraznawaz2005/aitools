24abe194acd0f60e02238d7120d0c4c732e0366d
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-26 23:45:29 +0500

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (!Schema::hasTable('bots')) {
-            Artisan::call('migrate');
             Artisan::call('db:seed');
         }
 
