5b48e2b9854eaf882ecaf643ad9c709bae2b2a71
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-26 23:49:57 +0500

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Exception;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Lorisleiva\CronTranslator\CronTranslator;
@@ -19,10 +17,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (!Schema::hasTable('bots')) {
-            Artisan::call('db:seed');
-        }
-
         config(['app.timezone' => System::timezone()]); // via nativephp
 
         $this->registerCustomValidators();
