ba8e33f724805d08f4b3c957c916b8af827875fe
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-30 13:58:28 +0500

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <div class="grid sm:grid-cols-3 lg:grid-cols-3 gap-6 md:gap-10" tabindex="-1">
             @foreach(config('tools') as $tool)
                 <a href="{{route($tool['route'])}}" wire:navigate wire:key="{{ $tool['name'] }}" tabindex="-1">
-                    <div class="size-full bg-white shadow-lg rounded-lg p-4 dark:bg-neutral-900 text-center hover:bg-gray-200">
+                    <div class="size-full bg-white shadow text-center hover:shadow-lg transition-shadow rounded-lg p-4">
                         <div class="inline-flex justify-center items-center">
                             <img width="75" height="75" alt="{{$tool['name']}}" src="{{$tool['icon_data']}}">
                         </div>
