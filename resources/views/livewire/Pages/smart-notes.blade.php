60bbc138120a87a9fbc3724344ddd1bc856f257f
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-30 18:25:02 +0500

    updated

--- resources/views/livewire/Pages/smart-notes.blade.php
+++ resources/views/livewire/Pages/smart-notes.blade.php
@@ -6,7 +6,7 @@
 
             <livewire:apikeys.api-key-banner/>
 
-            <div class="text-center font-medium text-gray-400 text-2xl p-2 h-screen items-center flex justify-center">
+            <div class="text-center font-medium text-gray-400 text-xl p-2 h-screen items-center flex justify-center">
                 <span
                     class="inline-flex items-center gap-x-1.5 py-3 px-6 rounded-full bg-gray-100 text-gray-500 dark:bg-yellow-800/30 dark:text-yellow-500">
                     You have total of {{ $this->totalNotesCount }} notes in {{ $this->folders->count() }} folders
