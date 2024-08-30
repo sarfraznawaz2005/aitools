96b719e14169443a4b05beb7026fa1141e826118
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-08-31 00:49:35 +0500

    updated

--- resources/views/livewire/Pages/smart-notes.blade.php
+++ resources/views/livewire/Pages/smart-notes.blade.php
@@ -8,7 +8,7 @@
 
             <div class="text-center font-medium text-gray-400 text-xl p-2 h-screen items-center flex justify-center">
                 <span
-                    class="inline-flex items-center gap-x-1.5 py-3 px-6 rounded-full bg-gray-100 text-gray-500 dark:bg-yellow-800/30 dark:text-yellow-500">
+                    class="inline-flex items-center gap-x-1.5 py-3 px-6 rounded-full bg-zinc-200 text-gray-500 dark:bg-yellow-800/30 dark:text-yellow-500">
                     You have total of {{ $this->totalNotesCount }} notes in {{ $this->folders->count() }} folders
 
                     <div x-data="{ open: false }" class="relative">
