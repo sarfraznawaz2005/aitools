81c9bff737c5eec2983090ff01a0adbb0dab2999
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-09-06 20:29:18 +0500

    updated

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 rem make sure "npm rund dev" command is run before this command; strange but it works
 
+rem had to comment out this line for app icon to show because that lib was using depraecated code:
+rem vendor/voku/portable-utf8/src/voku/helper/UTF8.php:536
+
 rem now build the app
 
 rem see cleanup command in routes\console.php
