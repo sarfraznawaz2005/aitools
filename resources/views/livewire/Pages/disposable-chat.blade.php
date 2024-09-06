4f4be962db976200bcd7120a3d75ed2abad05a37
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-09-07 02:12:19 +0500

    updated

--- resources/views/livewire/Pages/disposable-chat.blade.php
+++ resources/views/livewire/Pages/disposable-chat.blade.php
@@ -54,6 +54,67 @@
                 <div class="flex-grow border-t mx-2 border-purple-400"></div>
             </div>
 
+            <div class="flex justify-end w-full fixed top-10 right-10 gap-x-2">
+                <div x-data="{ open: false }" class="relative">
+                    <button
+                        @click="open = !open"
+                        type="button"
+                        class="py-2 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                    >
+                        <x-icons.export class="shrink-0 size-4"/>
+                        Export
+                        <svg
+                            class="size-4"
+                            :class="{ 'rotate-180': open }"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        >
+                            <path d="m6 9 6 6 6-6"/>
+                        </svg>
+                    </button>
+
+                    <div
+                        x-cloak
+                        x-show="open"
+                        @click.away="open = false"
+                        x-transition:enter="transition ease-out duration-200"
+                        x-transition:enter-start="opacity-0 scale-95"
+                        x-transition:enter-end="opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-150"
+                        x-transition:leave-start="opacity-100 scale-100"
+                        x-transition:leave-end="opacity-0 scale-95"
+                        class="absolute right-0 mt-0.5 min-w-28 bg-white shadow-lg space-y-0.5 divide-y divide-gray-200"
+                        role="menu"
+                    >
+                        <div class="py-2 first:pt-0 last:pb-0">
+                            <a
+                                @click.prevent="$wire.export('html')"
+                                class="flex items-center gap-x-3.5 py-2 px-3 text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                                href="#"
+                            >
+                                <x-icons.code class="shrink-0 size-4"/>
+                                HTML
+                            </a>
+                            <a
+                                @click.prevent="$wire.export('txt')"
+                                class="flex items-center gap-x-3.5 py-2 px-3 text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                                href="#"
+                            >
+                                <x-icons.text class="shrink-0 size-4"/>
+                                TEXT
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <!-- Chat content -->
             <div class="space-y-4 pt-2 px-4">
 
