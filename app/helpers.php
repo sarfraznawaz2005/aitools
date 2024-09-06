1cfcb4ea1f0b96128e7e68a2ef2d5ea45733f92a
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-09-07 02:26:34 +0500

    updated

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 /*
+ * jsonvectorstore for document bot
  * https://github.com/Doriandarko/gemini-ui-to-code
  * wikipedia research via tool calling - https://github.com/google-gemini/cookbook/blob/main/examples/Search_reranking_using_embeddings.ipynb
  * code execution - https://github.com/google-gemini/cookbook/blob/main/quickstarts/Code_Execution.ipynb
