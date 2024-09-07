c9dc1ddd20ee9cce52d9ee5c716250e748f27fbf
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-09-07 20:00:36 +0500

    updated

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
-rem make sure "npm rund dev" command is run before this command; strange but it works
-
 rem had to comment out this line for app icon to show because that lib was using depraecated code:
 rem vendor/voku/portable-utf8/src/voku/helper/UTF8.php:536
 
@@ -8,6 +6,6 @@ rem now build the app
 rem see cleanup command in routes\console.php
 php artisan cleanup
 
-npm run build && php artisan native:build win -v
+php artisan native:build win -v
 
 pause
