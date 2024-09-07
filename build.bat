9b07483173aac3f5c490a447d6af9767ba462b47
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-09-07 18:23:38 +0500

    updated

--- build.bat
+++ build.bat
@@ -8,6 +8,6 @@ rem now build the app
 rem see cleanup command in routes\console.php
 php artisan cleanup
 
-npm run build && php artisan native:build win
+npm run build && php artisan native:build win -v
 
 pause
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,17 @@
 <x-layouts.app :title="''">
-    <div class="mx-auto pt-24 px-8" tabindex="-1">
+    <div class="flex items-center justify-center min-h-screen mx-auto px-8" tabindex="-1">
         <x-flash/>
         <livewire:apikeys.api-key-banner />
 
-        <div class="grid sm:grid-cols-3 lg:grid-cols-3 gap-6 md:gap-10" tabindex="-1">
+        <div class="grid sm:grid-cols-4 lg:grid-cols-4 gap-6 md:gap-10">
             @foreach(config('tools') as $tool)
-                <a href="{{route($tool['route'])}}" wire:navigate.hover tabindex="-1">
+                <a href="{{route($tool['route'])}}" wire:navigate.hover>
                     <div class="size-full bg-white shadow text-center hover:shadow-lg transition-shadow rounded-lg p-4">
                         <div class="inline-flex justify-center items-center">
                             <img width="75" height="75" alt="{{$tool['name']}}" src="{{$tool['icon_data']}}">
                         </div>
                         <h3 class="block text-lg font-bold text-gray-500 mb-2 dark:text-white">{{$tool['name']}}</h3>
-                        <p class="text-gray-600">{{$tool['description']}}</p>
+                        <p class="text-gray-600 text-sm">{{$tool['description']}}</p>
                     </div>
                 </a>
             @endforeach
