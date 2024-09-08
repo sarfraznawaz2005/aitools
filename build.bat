1979a7eb771738de7333a60c60499354725c525e
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-09-08 16:31:12 +0500

    updated

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ rem now build the app
 rem see cleanup command in routes\console.php
 php artisan cleanup
 
-php artisan native:build win x64 -v
+php artisan native:build win x64 -v --no-interaction
 
 pause
