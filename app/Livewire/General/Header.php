1c2ab9d815fa2a7fccee05eca006298a36d898c6
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-09-09 23:57:57 +0500

    updated

--- app/Livewire/General/Header.php
+++ app/Livewire/General/Header.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\General;
 
 use Livewire\Attributes\On;
+use Livewire\Attributes\Renderless;
 use Livewire\Component;
 
 class Header extends Component
@@ -11,11 +12,13 @@ class Header extends Component
     public bool $showHeader = true;
 
     #[On('hideHeader')]
+    #[Renderless]
     public function hideHeader(): void
     {
         $this->showHeader = false;
     }
 
+    #[Renderless]
     public function mount($title): void
     {
         $this->title = $title;
