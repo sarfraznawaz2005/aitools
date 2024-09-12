56e3cdb7a743fe1593326a3512a2aa3dd92010ec
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-09-13 01:17:56 +0500

    updated

--- packages/doctalk/src/Views/components/layouts/sidebar.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Sticky Sidebar -->
-<div class="sidebar" id="sidebar">
-    <h2>Conversations</h2>
-    <ul>
-        <li><a href="#">Chat 1</a></li>
-        <li><a href="#">Chat 2</a></li>
-        <li><a href="#">Chat 3</a></li>
-    </ul>
-</div>
