5ba5321888efdd8bd58d2290b58474274c33f791
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2025-08-09 17:52:55 +0500

    updated

--- app/Console/Commands/CleanupCommand.php
+++ app/Console/Commands/CleanupCommand.php
@@ -8,25 +8,9 @@ use Illuminate\Support\Facades\File;
 
 class CleanupCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'cleanup';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Clear cache and temp files';
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
     public function handle()
     {
         $systemFolders = [
